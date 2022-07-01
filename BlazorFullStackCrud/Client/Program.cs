578124cc88ae82e0edd57f3ec21f849fd091f771
tahanasrollahii <tahanasrollahii@gmail.com>
2022-07-01 17:40:55 +0430

    Add project files.

---
+++ BlazorFullStackCrud/Client/Program.cs
@@ -0,0 +1,18 @@
+global using BlazorFullStackCrud.Client.Services.SuperHeroService;
+global using BlazorFullStackCrud.Shared;
+using BlazorFullStackCrud.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+await builder.Build().RunAsync();
